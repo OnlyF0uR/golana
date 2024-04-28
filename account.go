8c6af9ffc95f05423cb568db3c92047b47a4831c
OnlyF0uR <29165327+OnlyF0uR@users.noreply.github.com>
2024-04-28 10:44:46 +0200

    feat: Signing/verifying with account

--- account.go
+++ account.go
@@ -42,3 +42,21 @@ func AccountFromPrivateKeyBase58(privateKey string) (*Account, error) {
 
 	return AccountFromPrivateKey(key), nil
 }
+
+func (a *Account) SignMessage(payload []byte) (string, error) {
+	bytes, err := solcrypt.Sign(a.privateKey, payload)
+	if err != nil {
+		return "", err
+	}
+
+	return base58.Encode(bytes), nil
+}
+
+func (a *Account) VerifySignature(payload []byte, signature string) (bool, error) {
+	sig, err := base58.Decode(signature)
+	if err != nil {
+		return false, err
+	}
+
+	return solcrypt.VerifySig(a.publicKey, payload, sig)
+}
